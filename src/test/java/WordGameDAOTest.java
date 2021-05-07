fccf1dce3fdd05f279b3744a9de7305a649cb2b1
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-05-07 21:25:40 +0200

    ...

--- src/test/java/WordGameDAOTest.java
+++ src/test/java/WordGameDAOTest.java
@@ -2,9 +2,7 @@ import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,7 +20,6 @@ class WordGameDAOTest {
 
     @Test
     void testAddWord() {
-        List<String> strings = new ArrayList<>();
         wordGameDAO.addWords("John Doe");
         assertEquals(Arrays.asList("John Doe"), wordGameDAO.queryWords());
     }
