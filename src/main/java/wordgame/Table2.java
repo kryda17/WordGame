89099a4fcc4b54b06e2ad97dfd56281777bab1d7
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-05-08 16:39:22 +0200

    ..

---
+++ src/main/java/wordgame/Table2.java
@@ -0,0 +1,85 @@
+package wordgame;
+
+import java.util.*;
+
+public class Table2 {
+
+    private static final int GRID_SIZE = 20;
+    private static final String BLACK_GRID_PLACEHOLDER = "####";
+    private static final int MIN_BLACK_SQUARES = GRID_SIZE * GRID_SIZE / 10;
+    private static final Random RND = new Random();
+    private static final int NUM_OF_BLACK_SQUARES = MIN_BLACK_SQUARES + RND.nextInt(MIN_BLACK_SQUARES);
+
+    private List<Coordinate> coordinates = new ArrayList<>();
+    private String[][] table;
+
+    public Table2() {
+        table = new String[GRID_SIZE][GRID_SIZE];
+        makeBlackSquares();
+    }
+
+    private void makeBlackSquares() {
+        for (int i = 0; i < NUM_OF_BLACK_SQUARES; i++) {
+            generateCoordinate();
+        }
+        for (Coordinate coord: coordinates) {
+            table[coord.getxCoord()][coord.getyCoord()] = BLACK_GRID_PLACEHOLDER;
+        }
+    }
+
+    private void generateCoordinate() {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            int x = RND.nextInt(GRID_SIZE);
+            int y = i;
+            Coordinate coord = new Coordinate(x, y);
+
+            if (isGeneratedCoordPosGood(coord)) {
+                //return coord;
+                coordinates.add(coord);
+            }
+        }
+    }
+
+    private boolean isGeneratedCoordPosGood(Coordinate coordinate) {
+        int x = coordinate.getxCoord();
+        int y = coordinate.getyCoord();
+
+        /*if (x == 1 || x == GRID_SIZE - 2 || y == 1 || y == GRID_SIZE - 2) {
+            return false;
+        }
+         */
+
+        for (int i = 0; i < coordinates.size(); i++) {
+            Coordinate secCoord = coordinates.get(i);
+
+            int xDiff = Math.abs(x - secCoord.getxCoord());
+            int yDiff = Math.abs(y - secCoord.getyCoord());
+
+            if ((xDiff == 0 && yDiff < 3) || (yDiff == 0 && xDiff < 3)) {
+                return false;
+            }
+        }
+
+        //coordinates.add(coordinate);
+        return true;
+    }
+
+    public void printTable() {
+        for (String[] str : table) {
+            for (String s : str) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*private boolean isAllRowColContainsBlackSquare() {
+        for (Coordinate item : coordinates) {
+            int x = item.getxCoord();
+            int y = item.getyCoord();
+            if ()
+        }
+    }
+
+     */
+}
---
+++ src/test/java/wordgame/Table2Test.java
@@ -0,0 +1,17 @@
+package wordgame;
+
+import org.junit.jupiter.api.Test;
+
+    class Table2Test {
+
+        @Test
+        void testPrint(){
+            Table2 table = new Table2();
+            table.printTable();
+        }
+
+
+
+
+
+    }
