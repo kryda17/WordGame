a4fe69b8d0890f2641be08a835dfc97766bbb349
Herko <herko@index.hu>
2021-05-08 12:19:20 +0200

    tableGenerator 3.0

--- src/main/java/wordgame/Table.java
+++ src/main/java/wordgame/Table.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class Table {
 
-    private List<Coordinate> coordinates = new ArrayList<>();
 
     private static final int GRID_SIZE = 10;
     private static final String BLACK_GRID_PLACEHOLDER = "#";
@@ -14,11 +13,25 @@ public class Table {
     private static final int RANDOM_PLUS_BLACK_SQUARES = 10;
     private static final Random RND = new Random();
 
+    private List<Coordinate> coordinates;
     private String[][] table;
 
     public Table() {
         table = new String[GRID_SIZE][GRID_SIZE];
+        fillCoordinateListWithInvalidSqaures();
         makeBlackSquares();
+    }
+
+    private void fillCoordinateListWithInvalidSqaures() {
+        List<Coordinate> toAdd = List.of(new Coordinate(0, 1),
+                new Coordinate(0, GRID_SIZE - 2),
+                new Coordinate(1, 0),
+                new Coordinate(1, GRID_SIZE - 1),
+                new Coordinate(GRID_SIZE - 2, 0),
+                new Coordinate(GRID_SIZE - 2, GRID_SIZE - 1),
+                new Coordinate(GRID_SIZE - 1, GRID_SIZE - 2),
+                new Coordinate(GRID_SIZE - 1, 1));
+        coordinates = new ArrayList<>(toAdd);
     }
 
     private void makeBlackSquares() {
@@ -29,27 +42,28 @@ public class Table {
         }
     }
 
-    private boolean isGeneratedCoordPosGood(Coordinate coordinate) {
-        for (int i = 0;i < coordinates.size(); i++) {
-            Coordinate secCoord = coordinates.get(i);
-            int xDiff = Math.abs(coordinate.getxCoord() - secCoord.getxCoord());
-            int yDiff = Math.abs(coordinate.getyCoord() - secCoord.getyCoord());
-            if (xDiff < 2 && yDiff < 2) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     private Coordinate generateCoordinate() {
         int xCoord = RND.nextInt(GRID_SIZE);
         int yCoord = RND.nextInt(GRID_SIZE);
         Coordinate coord = new Coordinate(xCoord, yCoord);
 
-        if (isValidCoord(coord)) {
+        if (isGeneratedCoordPosGood(coord)) {
             return coord;
         }
-         return generateCoordinate();
+        return generateCoordinate();
+    }
+
+    private boolean isGeneratedCoordPosGood(Coordinate coordinate) {
+        for (int i = 0; i < coordinates.size(); i++) {
+            Coordinate secCoord = coordinates.get(i);
+            int xDiff = Math.abs(coordinate.getxCoord() - secCoord.getxCoord());
+            int yDiff = Math.abs(coordinate.getyCoord() - secCoord.getyCoord());
+            if (xDiff < 2 || yDiff < 2) {
+                return false;
+            }
+        }
+        coordinates.add(coordinate);
+        return true;
     }
 
     private boolean isValidCoord(Coordinate coord) {
