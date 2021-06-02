9a119e8cd5e4f21125a201cd1577cd6cc7a2c0f8
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-06-03 01:36:45 +0200

    Alpha 6.0

--- src/test/java/wordgame/chris/jpa/Table2Test.java
+++ src/test/java/wordgame/chris/jpa/Table2Test.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Table2Test {
 
-    private Table2 table2;
+    private FillTable fillTable;
     private WordGameJpaDAO wordGameJpaDAO;
 
     @BeforeEach
@@ -21,7 +21,7 @@ class Table2Test {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("wordgame-jpa");
         wordGameJpaDAO = new WordGameJpaDAO(entityManagerFactory);
         MariaDbDataSource mDs = MDataSource.getMariaDbDataSource();
-        table2 = new Table2(new Random(1));
+        fillTable = new FillTable(new Table2(15 ,new Random(1)));
         Flyway flyway = Flyway.configure().dataSource(mDs).load();
         flyway.clean();
         flyway.migrate();
@@ -31,31 +31,31 @@ class Table2Test {
 
     @Test
     void testFunc() {
-        table2.func();
-        table2.printTable();
+        fillTable.func(fillTable.getTable());
+        fillTable.printTable();
     }
 
     @Test
     void testPrint() {
-        table2.printTable();
+        fillTable.printTable();
     }
 
     @Test
     void likePatternMakerTest() {
-        String likePattern = table2.likePatternMaker(new WordStartingCoordinate(0,1, Alignment.HORISONTAL));
+        String likePattern = fillTable.likePatternMaker(new WordStartingCoordinate(0,1, Alignment.HORISONTAL));
         assertEquals("_", likePattern);
 
     }
 
     @Test
     void wordLengthFromStartingCoordTest() {
-        int wordLength = table2.wordLengthFromStartingCoordinate(new WordStartingCoordinate(1,0,  Alignment.HORISONTAL));
+        int wordLength = fillTable.wordLengthFromStartingCoordinate(new WordStartingCoordinate(1,0,  Alignment.HORISONTAL));
         assertEquals(2, wordLength);
-        wordLength = table2.wordLengthFromStartingCoordinate(new WordStartingCoordinate(13,0, Alignment.HORISONTAL));
+        wordLength = fillTable.wordLengthFromStartingCoordinate(new WordStartingCoordinate(13,0, Alignment.HORISONTAL));
         assertEquals(2, wordLength);
-        wordLength = table2.wordLengthFromStartingCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL));
+        wordLength = fillTable.wordLengthFromStartingCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL));
         assertEquals(8, wordLength);
-        wordLength = table2.wordLengthFromStartingCoordinate(new WordStartingCoordinate(12,13, Alignment.VERTICAL));
+        wordLength = fillTable.wordLengthFromStartingCoordinate(new WordStartingCoordinate(12,13, Alignment.VERTICAL));
         assertEquals(2, wordLength);
 
 
@@ -63,45 +63,45 @@ class Table2Test {
 
     @Test
     void iswordWritableTest() {
-        assertTrue(table2.isWordWritable("HU", new WordStartingCoordinate(1,0, Alignment.HORISONTAL)));
-        assertFalse(table2.isWordWritable("DANIELLA", new WordStartingCoordinate(1,0, Alignment.VERTICAL)));
-        assertFalse(table2.isWordWritable("HU", new WordStartingCoordinate(0,1, Alignment.HORISONTAL)));
+        assertTrue(fillTable.isWordWritable("HU", new WordStartingCoordinate(1,0, Alignment.HORISONTAL)));
+        assertFalse(fillTable.isWordWritable("DANIELLA", new WordStartingCoordinate(1,0, Alignment.VERTICAL)));
+        assertFalse(fillTable.isWordWritable("HU", new WordStartingCoordinate(0,1, Alignment.HORISONTAL)));
     }
 
 
     @Test
     void wordWriterTest() {
-        table2.fillWordFromCoordinate("YK", new WordStartingCoordinate(0,1, Alignment.VERTICAL));
-        table2.fillWordFromCoordinate("US", new WordStartingCoordinate(13,0, Alignment.HORISONTAL));
-        table2.printTable();
-        assertEquals("Y", table2.readCharacterAtCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL)));
-        assertEquals("K", table2.readCharacterAtCoordinate(new WordStartingCoordinate(0,2, Alignment.VERTICAL)));
-        assertEquals("U", table2.readCharacterAtCoordinate(new WordStartingCoordinate(13,0, Alignment.HORISONTAL)));
-        assertEquals("S", table2.readCharacterAtCoordinate(new WordStartingCoordinate(14,0, Alignment.HORISONTAL)));
+        fillTable.fillWordFromCoordinate("YK", new WordStartingCoordinate(0,1, Alignment.VERTICAL));
+        fillTable.fillWordFromCoordinate("US", new WordStartingCoordinate(13,0, Alignment.HORISONTAL));
+        fillTable.printTable();
+        assertEquals("Y", fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL)));
+        assertEquals("K", fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(0,2, Alignment.VERTICAL)));
+        assertEquals("U", fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(13,0, Alignment.HORISONTAL)));
+        assertEquals("S", fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(14,0, Alignment.HORISONTAL)));
     }
 
     @Test
     void readCharAtCoordTest() {
-        assertEquals(Table2.BLACK_GRID_PLACEHOLDER, table2.readCharacterAtCoordinate(new WordStartingCoordinate(0,0, Alignment.VERTICAL)));
-        assertEquals(Table2.EMPTY_GRID_PLACEHOLDER, table2.readCharacterAtCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL)));
+        assertEquals(fillTable.getTable().BLACK_GRID_PLACEHOLDER, fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(0,0, Alignment.VERTICAL)));
+        assertEquals(fillTable.getTable().EMPTY_GRID_PLACEHOLDER, fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL)));
 
     }
 
     @Test
     void insertCharacterTest() {
-        table2.insertCharacter("Y", new WordStartingCoordinate(1, 0, Alignment.HORISONTAL));
-        assertEquals("Y", table2.readCharacterAtCoordinate(new WordStartingCoordinate(1, 0, Alignment.HORISONTAL)));
-        table2.insertCharacter("Y", new WordStartingCoordinate(1, 0, Alignment.HORISONTAL));
-        assertThrows(IllegalStateException.class, () -> {table2.insertCharacter("X", new WordStartingCoordinate(1, 0, Alignment.HORISONTAL));   });
-        assertThrows(IllegalStateException.class, () -> {table2.insertCharacter("X", new WordStartingCoordinate(0, 0, Alignment.HORISONTAL));   });
+        fillTable.insertCharacter("Y", new WordStartingCoordinate(1, 0, Alignment.HORISONTAL));
+        assertEquals("Y", fillTable.readCharacterAtCoordinate(new WordStartingCoordinate(1, 0, Alignment.HORISONTAL)));
+        fillTable.insertCharacter("Y", new WordStartingCoordinate(1, 0, Alignment.HORISONTAL));
+        assertThrows(IllegalStateException.class, () -> {fillTable.insertCharacter("X", new WordStartingCoordinate(1, 0, Alignment.HORISONTAL));   });
+        assertThrows(IllegalStateException.class, () -> {fillTable.insertCharacter("X", new WordStartingCoordinate(0, 0, Alignment.HORISONTAL));   });
     }
 
     @Test
     void getWordTest() {
-        table2.fillWordFromCoordinate("HU", new WordStartingCoordinate(1,0, Alignment.HORISONTAL));
-        assertEquals("HU", table2.getWordFromStartingCoordinate(new WordStartingCoordinate(1,0, Alignment.HORISONTAL)));
-        table2.fillWordFromCoordinate("DANIELLA", new WordStartingCoordinate(0,1, Alignment.VERTICAL));
-        assertEquals("DANIELLA", table2.getWordFromStartingCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL)));
+        fillTable.fillWordFromCoordinate("HU", new WordStartingCoordinate(1,0, Alignment.HORISONTAL));
+        assertEquals("HU", fillTable.getWordFromStartingCoordinate(new WordStartingCoordinate(1,0, Alignment.HORISONTAL)));
+        fillTable.fillWordFromCoordinate("DANIELLA", new WordStartingCoordinate(0,1, Alignment.VERTICAL));
+        assertEquals("DANIELLA", fillTable.getWordFromStartingCoordinate(new WordStartingCoordinate(0,1, Alignment.VERTICAL)));
     }
 
 }
