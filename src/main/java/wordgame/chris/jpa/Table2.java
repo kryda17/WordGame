f486acf8feaa3fa18a4e17524706c9d7698337d4
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-06-03 17:32:01 +0200

    ....

--- src/main/java/wordgame/chris/jpa/Table2.java
+++ src/main/java/wordgame/chris/jpa/Table2.java
@@ -7,12 +7,13 @@ public class Table2 {
     public final int GRID_SIZE;
     public final String EMPTY_GRID_PLACEHOLDER = "0";
     public final String BLACK_GRID_PLACEHOLDER = "#";
+    private final int BLACKS_MIN_DISTANCE = 2;
     private final int MIN_BLACK_SQUARES; //Hogy minden sorban és oszlopban legyen egy,az egyenlő a GRID_SIZE
     private final int MIN_ADDITIONAL_BLACK_SQUARE;
 
     private String[][] table;
     private Random rnd;
-    private List<Coordinate> blackCoordinates = new ArrayList<>();
+
 
     public Table2(int SIZE, Random rnd) {
         GRID_SIZE = SIZE;
@@ -31,7 +32,7 @@ public class Table2 {
     private void fillTalbeWithEmptys() {
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
-                deleteCoordinate(new Coordinate(i, j));
+                deleteCoordinate(i, j);
             }
         }
     }
@@ -39,13 +40,108 @@ public class Table2 {
     private void makeBlackSquares() {
         generateRndBlacks();
         generateAdditionalRandomBlacks();
-        placeAllGeneratedBlacks();
     }
 
-    private void placeAllGeneratedBlacks() {
-        for (int i = 0; i < blackCoordinates.size(); i++) {
-            insertBlackSquare(blackCoordinates.get(i));
+
+
+    private void generateAdditionalRandomBlacks() {
+        int rnd_num_of_black_squares = rnd.nextInt(MIN_BLACK_SQUARES) + MIN_ADDITIONAL_BLACK_SQUARE;
+        int allRequiredBlackSquare = MIN_BLACK_SQUARES + rnd_num_of_black_squares;
+        for (int i = 0; i < allRequiredBlackSquare; i++) {
+            genarateBlack();
         }
+    }
+
+    private void genarateBlack() {
+        int x = rnd.nextInt(GRID_SIZE);
+        int y = rnd.nextInt(GRID_SIZE);
+        Coordinate coordinate = new Coordinate(x,y);
+        if (isGeneratedCoordDifferenceMinTwoV3(coordinate)) {
+            insertBlackSquare(coordinate);
+        }
+    }
+
+    /*private boolean isGeneratedCoordDifferenceMinTwo(Coordinate coordinate) {
+        int x = coordinate.getX();
+        int y = coordinate.getY();
+
+        for (Coordinate secCoord : blackCoordinates) {
+            int xDiff = Math.abs(x - secCoord.getX());
+            int yDiff = Math.abs(y - secCoord.getY());
+            if ((xDiff == 0 && yDiff < 3) || (yDiff == 0 && xDiff < 3)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+     */
+
+
+    private void generateRndBlacks() {
+        //Ha GRID_SIZE fekete van és csak egyetlen egy van minden sorban és oszlopban,akk nincs üres sor
+        for (int i = 0; i < GRID_SIZE; i++) {
+            while (true) {
+                int x = rnd.nextInt(GRID_SIZE);
+                int y = i;
+                Coordinate coordinate = new Coordinate(x,y);
+                if (!isRowOrColAlreadyContainsBlackV2(coordinate) && isGeneratedCoordDifferenceMinTwoV3(coordinate)) {
+                    insertBlackSquare(coordinate);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    private void insertBlackSquare(Coordinate coord) {
+        int x = coord.getX();
+        int y = coord.getY();
+        if (isCoordinateBlack(coord)) {
+            throw new IllegalStateException("Fekete kockára írás hiba.");
+        }
+        table[y][x] = BLACK_GRID_PLACEHOLDER;
+    }
+
+    public boolean isGeneratedCoordDifferenceMinTwoV3(Coordinate coordinate) {
+        for (int i = -BLACKS_MIN_DISTANCE; i <= BLACKS_MIN_DISTANCE ; i++) {
+            int tempX = coordinate.getX() + i;
+            if (tempX < 0) {
+                continue;
+            }
+            if (tempX >= GRID_SIZE) {
+                break;
+            }
+            System.out.println(coordinate.getX() + i + ":" + coordinate.getY());
+            if (isCoordinateBlack(coordinate.getX() + i, coordinate.getY())) {
+                return false;
+            }
+        }
+        for (int j = -BLACKS_MIN_DISTANCE; j < BLACKS_MIN_DISTANCE; j++) {
+            int tempY = coordinate.getY() + j;
+            if (tempY < 0) {
+                continue;
+            }
+            if (tempY >= GRID_SIZE) {
+                break;
+            }
+            System.out.println( coordinate.getX() + ":" + (coordinate.getY() + j));
+            if (isCoordinateBlack(coordinate.getX(), coordinate.getY() + j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private boolean isRowOrColAlreadyContainsBlackV2(Coordinate coord) {
+        for (int i = 0;i < GRID_SIZE; i++) {
+            if (isCoordinateBlack(i, coord.getY()) || isCoordinateBlack(coord.getX(), i)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean isCoordinateBlack(Coordinate coord) {
@@ -64,81 +160,11 @@ public class Table2 {
         return false;
     }
 
-    private void insertBlackSquare(Coordinate coord) {
-        int x = coord.getX();
-        int y = coord.getY();
-        if (isCoordinateBlack(coord)) {
-            throw new IllegalStateException("Fekete kockára írás hiba.");
+    public void deleteCoordinate(int x,int y) {
+        if (isCoordinateBlack(x, y)) {
+            throw new IllegalStateException("Can't delete black coordinate.");
         }
-        table[y][x] = BLACK_GRID_PLACEHOLDER;
-    }
-
-
-
-    private void generateAdditionalRandomBlacks() {
-        int counter = 0; //ideiglenes
-        int rnd_num_of_black_squares = rnd.nextInt(MIN_BLACK_SQUARES) + MIN_ADDITIONAL_BLACK_SQUARE;
-        int allRequiredBlackSquare = MIN_BLACK_SQUARES + rnd_num_of_black_squares;
-        while (blackCoordinates.size() < allRequiredBlackSquare) {
-            ++counter; //ideiglenes
-            int x = rnd.nextInt(GRID_SIZE);
-            int y = rnd.nextInt(GRID_SIZE);
-            Coordinate coordinate = new Coordinate(x,y);
-            if (isGeneratedCoordDifferenceMinTwo(coordinate)) {
-                blackCoordinates.add(coordinate);
-            }
-        }
-        System.out.println(rnd_num_of_black_squares + " random fekete kocka generálása pluszba még: " +       //ideiglenes
-                + counter + " iteráció ---  Koordinátákat tartalmazó lista mérete: " + blackCoordinates.size());    //ideiglenes
-        System.out.println();  //ideiglenes
-    }
-
-
-
-    private boolean isGeneratedCoordDifferenceMinTwo(Coordinate coordinate) {
-        int x = coordinate.getX();
-        int y = coordinate.getY();
-
-        for (Coordinate secCoord : blackCoordinates) {
-            int xDiff = Math.abs(x - secCoord.getX());
-            int yDiff = Math.abs(y - secCoord.getY());
-            if ((xDiff == 0 && yDiff < 3) || (yDiff == 0 && xDiff < 3)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isRowColAlreadyContainsBlack(Coordinate coord) {
-        for (Coordinate secCoord : blackCoordinates) {
-            if ((coord.getX() == secCoord.getX()) || (coord.getY() == secCoord.getY())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void generateRndBlacks() {
-        int counter = 0; //ideiglenes
-        //Ha GRID_SIZE fekete van és csak egyetlen egy van minden sorban és oszlopban,akk nincs üres sor
-        for (int i = 0; i < GRID_SIZE; i++) {
-            while (true) {
-                ++counter; //ideiglenes
-                int x = rnd.nextInt(GRID_SIZE);
-                int y = i;
-                Coordinate coordinate = new Coordinate(x,y);
-                if (!isRowColAlreadyContainsBlack(coordinate) && isGeneratedCoordDifferenceMinTwo(coordinate)) {
-                    blackCoordinates.add(coordinate);
-                    break;
-                }
-            }
-        }
-        System.out.println(GRID_SIZE + " fekete kocka: " + counter +    //ideiglenes
-                " iteráció ---  Koordinátákat tartalmazó lista mérete: " + blackCoordinates.size());     //ideiglenes
-    }
-
-    public void deleteCoordinate(Coordinate coordinate) {
-        table[coordinate.getY()][coordinate.getX()] = EMPTY_GRID_PLACEHOLDER;
+        table[y][x] = EMPTY_GRID_PLACEHOLDER;
     }
 
     public String[][] getTable() {
